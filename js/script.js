6c573082b169b9b5468695e40c83f398538137b6
fernandodearrillaga <115628468+fernandodearrillaga@users.noreply.github.com>
2022-11-10 18:42:44 +0100

    10/11/22 18:42

--- js/script.js
+++ js/script.js
@@ -1,36 +1,28 @@
-//window.onload = funcion();
-/*var numeroTarjeta=document.getElementById("numeroTarjeta");
-var direccion=document.getElementById("direccion");
-direccion.onclick = activarTarjeta();
-function activarTarjeta(){
-    numeroTarjeta.removeAttribute("disabled");
-}*/
+window.addEventListener("load",iniciar);
+function iniciar() { //añade eventos a los campos dirección y país
+    document.getElementById('direccion').addEventListener('keyup', activarTarjeta);
+    document.getElementById('pais').addEventListener('click', activarTarjeta);
+}
+
+
 
 
 const regexp=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
-const regexp2=/^$/;
-const regexpCorreo=/^[\w]+@[\w]+.[\w]+$/;
-const regexpCorreo2=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const regexpCorreo=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const regexpNombre=/^[\w]+[\s]?[\w]+$/;
 const regexpTarjeta=/^\d{8,19}$/;
 
-//var password= document.getElementById("contra").value;
-//console.log(password);
 
-function validar(e) {
+
+function validar(e) {       //Valida los datos del registro
     e.preventDefault();
     var nombre=document.getElementById("nombre").value;
     var apellidos=document.getElementById("apellidos").value;
     var password= document.getElementById("contra").value;
     var password2=document.getElementById("contra2").value;
     var correo= document.getElementById("correo").value;
-    console.log(nombre);
-    console.log(regexpNombre.test(nombre));
-    console.log(regexpNombre.test(apellidos));
-    console.log(password);
-    console.log(regexp.test(password));
-    console.log(correo);
-    console.log(regexpCorreo.test(correo));
+    var tarjeta= document.getElementById("numeroTarjeta").value;
+
     
     if(regexpNombre.test(nombre)===false){
         alert("Nombre debe tener un máximo de 2 palabras");
@@ -43,38 +35,42 @@ function validar(e) {
         }
     else if(password!==password2){
         alert("Las contraseñas no coinciden");
+    } 
+    else if(regexpCorreo.test(correo)===false){
+        alert("El correo es inválido");
+    } 
+
+    else if (tarjeta!=""){
+        if(regexpTarjeta.test(tarjeta)===false){
+            alert("El número de tarjeta no es válido");
+        }
     }
-    else /*if (regexp.test(password)===true&&regexpCorreo.test(correo)===true&&password===password2)*/{
+    
+    else {
         alert("Se ha registrado correctamente");
     }
     
 }
 
-function validarLogin(e) {
+function validarLogin(e) {  //valida los datos del login
     e.preventDefault();
 
 
     var passwordLog= document.getElementById("contraLog").value;
     var correoLog= document.getElementById("correoLog").value;
-    console.log(passwordLog);
-    console.log(regexp.test(passwordLog));
-    console.log(correoLog);
-    console.log(regexpCorreo.test(correoLog));
     if (regexp.test(passwordLog)===true&&regexpCorreo.test(correoLog)===true){
         alert("Se ha logueado correctamente");
     }
 }
 
-function activarTarjeta() {
+function activarTarjeta() { //activa el campo del número de tarjeta de crédito
     var direccion= document.getElementById("direccion").value;
     var pais= document.getElementById("pais").value;
-    //console.log("aaa "+ pais);
     if (direccion.length >= 1  && pais.length >=1) {
       document.getElementById('numeroTarjeta').disabled = false;
     } else {
       document.getElementById('numeroTarjeta').disabled = true;
     }
 }
-document.getElementById('direccion').addEventListener('keyup', activarTarjeta);
-document.getElementById('pais').addEventListener('click', activarTarjeta);
+
 
